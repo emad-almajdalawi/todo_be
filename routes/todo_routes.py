20c41433291726c4d3aa925e0237824ed06c182a
emad-almajdalawi <e.almajdalawi@gamil.com>
2022-10-05 14:46:53 +0300

    fix: secondary crud

--- routes/todo_routes.py
+++ routes/todo_routes.py
@@ -1,7 +1,7 @@
 import json
 from flask import jsonify, request
 from bson import json_util
-from services.mogo_crud import get_data, post_task, delete_task, update_task
+from services.mogo_crud import delete_done, get_data, post_task, delete_task, task_done, update_task, delete_all, all_tasks_done
 
 
 def serialize(data):
@@ -85,7 +85,7 @@ def todo_routes(app):
     @app.route('/task/update/<id>', methods = ['POST'])
     def todo_update_task(id):
         """
-        Calls the CRUD methods to update the value of a task
+        Calls the CRUD methods to update the title of a task
 
         Args:
             - id: the id of the task to show its details
@@ -115,3 +115,57 @@ def todo_routes(app):
         delete_task(app.mongo, 'tasks', id)
 
         return jsonify(message='Success', status='202', data= serialised), 202
+
+
+    @app.route('/task/deleteall', methods = ['POST'])
+    def todo_delete_all():
+        """
+        Calls the CRUD methods to delete all tasks
+        """
+        delete_all(app.mongo, 'tasks')
+
+        return jsonify(message='Success', status='202'), 202
+
+
+    @app.route('/task/done/<id>', methods = ['POST'])
+    def todo_update_done(id):
+        """
+        Calls the CRUD methods to update the (done value) of a task
+
+        Args:
+            - id: the id of the task to show its details
+        """
+        req = request.get_json() #or requeist.args.get , or requist.form.get
+        done = req['done']
+                     
+        task_done(app.mongo, 'tasks', done, id)
+
+        updated_doc = list(get_data(app.mongo, 'tasks',id))[0]
+        serialised = serialize(updated_doc)
+
+        return jsonify(message = 'Success', status = '201', data = serialised), 201
+
+    
+    @app.route('/task/alldone/<is_done>', methods = ['POST'])
+    def todo_update_all_done(is_done):
+        """
+        Calls the CRUD methods to update the (done value) of all tasks to be true (done)
+        """
+        if is_done in ['true', 'True']:
+            is_done = True
+        if is_done in ['false', 'False']:
+            is_done = False
+        all_tasks_done(app.mongo, 'tasks', is_done)
+
+        return jsonify(message = 'Success', status = '201'), 201
+
+
+    @app.route('/task/deletedone', methods = ['POST'])
+    def todo_delete_done():
+        """
+        Calls the CRUD methods to delete done tasks
+        """
+        delete_done(app.mongo, 'tasks')
+
+        return jsonify(message='Success', status='202'), 202
+
--- services/mogo_crud.py
+++ services/mogo_crud.py
@@ -40,7 +40,7 @@ def post_task(mongo, collection, new_doc):
 
 def update_task(mongo, collection, new_title, id): # insted title and done you can pass new_doc
     """
-    Updates the value of a document in a collection in the datbase
+    Updates the title of a document in a collection in the datbase
     
     Args:
         - mongo (Object): pyMongo object of the database
@@ -59,7 +59,7 @@ def update_task(mongo, collection, new_title, id): # insted title and done you c
 
 def delete_task(mongo, collection, id):
     """
-    Updates the value of a document in a collection in the datbase
+    Delete a document in a collection in the datbase
 
     Args:
         - mongo (Object): pyMongo object of the database
@@ -73,3 +73,64 @@ def delete_task(mongo, collection, id):
         str(id)
 
     mongo.db[collection].delete_one({'_id': ObjectId(id)})
+
+
+def delete_all(mongo, collection):
+    """
+    Delete all documents in a collection in the datbase
+
+    Args:
+        - mongo (Object): pyMongo object of the database
+        - collection (String): collection name
+    """
+    if type(collection) != str:
+        str(collection)
+    
+    mongo.db[collection].delete_many({})
+
+
+def task_done(mongo, collection, new_done, id):
+    """
+    Updates the (done value) of a document in a collection in the datbase
+    
+    Args:
+        - mongo (Object): pyMongo object of the database
+        - collection (String): collection name
+        - new_doc (Dict): The new document
+        - id (string): The id of the document
+    """
+    if type(collection) != str:
+        str(collection)
+
+    if type(id) != str:
+        str(id)
+
+    mongo.db[collection].update_one({'_id': ObjectId(id)}, {'$set': {'done': new_done}})
+
+
+def all_tasks_done(mongo, collection, is_done):
+    """
+    Updates the (done value) of all documents in a collection in the datbase to be true (done)
+    
+    Args:
+        - mongo (Object): pyMongo object of the database
+        - collection (String): collection name
+    """
+    if type(collection) != str:
+        str(collection)
+
+    mongo.db[collection].update_many({}, {'$set': {'done': is_done}})
+
+
+def delete_done(mongo, collection):
+    """
+    Delete all done tsks
+
+    Args:
+        - mongo (Object): pyMongo object of the database
+        - collection (String): collection name
+    """
+    if type(collection) != str:
+        str(collection)
+    
+    mongo.db[collection].delete_many({'done': True})
